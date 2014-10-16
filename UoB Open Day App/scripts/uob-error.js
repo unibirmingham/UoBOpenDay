bb7a0ba531ae0f8a455790996c95696b6d5f11d9
M Nixon <m.nixon@bham.ac.uk>
2014-10-16 16:50:29 +0000

    Updated to only ask once if a user wants to send error information.

--- UoB Open Day App/scripts/uob-error.js
+++ UoB Open Day App/scripts/uob-error.js
@@ -3,6 +3,10 @@
     var uob = global.uob = global.uob || {};
     var app = global.app = global.app || {};
     uob.error = uob.error || {};
+    uob.log = uob.log || {};
+    
+    var postErrorMessage = null;
+    
     
     //Handler for the global object
     global.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
@@ -33,35 +37,61 @@
     uob.error.sendErrorMessageToUoB = function(name, message, filename, lineNumber, columnNumber, stack)
     {
         try{
-        navigator.notification.confirm("There has been an error. Would you like to send information about the error and your phone model and operating system to help the University of Birmingham prevent this error in future?", 
-            function(buttonIndex){
-                    if (buttonIndex===1){
-                        
-                        var device = global.device.model;
-                        var platform = global.device.platform;
-                        var version = global.device.version;
-                        
-                        $j.post("http://uob-mob-report.bham.ac.uk/report.aspx",
-                                {
-                                application: encodeValue(app.uobApplicationName),
-                                device: encodeValue(device), 
-                            	platform: encodeValue(platform), 
-                            	version: encodeValue(version), 
-                            	name: encodeValue(name), 
-                            	message: encodeValue(message), 
-                            	filename: encodeValue(filename), 
-                            	lineNumber: encodeValue(lineNumber), 
-                            	columnNumber: encodeValue(columnNumber), 
-                            	stack: encodeValue(stack)});
-                    }
-                },
-            'Send error data to UoB?','Send data, Cancel');
-    	}
+        
+            if (postErrorMessage===null)
+            {
+                postErrorMessage=false;
+                navigator.notification.confirm("There has been an error. Would you like to send information about the error and your phone model and operating system to help the University of Birmingham prevent this error in future?", 
+                    function(buttonIndex){
+                        if (buttonIndex===1){
+                            postErrorMessage = true;
+                            postErrorMessageToUoB(name, message, filename, lineNumber, columnNumber, stack);
+                                 }
+                    },
+                    'Send error data to UoB?','Send data, Cancel');
+        	}
+            else{
+                if (postErrorMessage){
+                    postErrorMessageToUoB(name, message, filename, lineNumber, columnNumber, stack);
+                }
+                else{
+                    logErrorSummary("Unposted error:" + name, message, filename, lineNumber, columnNumber, stack);
+                }
+            }
+        }
         catch(err)
         {
-            //just ignore this and return out
-            
+            logErrorSummary("Posting error:" + name, message, filename, lineNumber, columnNumber, stack);
         }
+    }
+    
+    var logErrorSummary = function(name, message, filename, lineNumber, columnNumber, stack)
+    {
+        uob.log.addLogError("Error: Name: " + name + ", message: " + message + ", filename: " + filename + ", line: " + lineNumber + ", column: " + columnNumber + ", stack: " + stack);
+    };
+    
+    var postErrorMessageToUoB = function(name, message, filename, lineNumber, columnNumber, stack)
+    {
+        
+        var device = global.device.model;
+        var platform = global.device.platform;
+        var version = global.device.version;
+        
+        $j.post("http://uob-mob-report.bham.ac.uk/report.aspx",
+                {
+                application: encodeValue(app.uobApplicationName),
+                device: encodeValue(device), 
+            	platform: encodeValue(platform), 
+            	version: encodeValue(version), 
+            	name: encodeValue(name), 
+            	message: encodeValue(message), 
+            	filename: encodeValue(filename), 
+            	lineNumber: encodeValue(lineNumber), 
+            	columnNumber: encodeValue(columnNumber), 
+            	stack: encodeValue(stack)});
+
+        logErrorSummary("Sending message about:" + name, message, filename, lineNumber, columnNumber, stack);
+        
     }
     
     var encodeValue = function(value)
